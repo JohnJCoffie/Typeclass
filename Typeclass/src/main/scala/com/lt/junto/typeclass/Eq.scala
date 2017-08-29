2e2402f3ca951fb6248e0a9b6278fb25d3205aa3
John Coffie <j_coffie@yahoo.com>
2017-08-28 20:18:25 -0500

    implemented adapter

--- Typeclass/src/main/scala/com/lt/junto/typeclass/Eq.scala
+++ Typeclass/src/main/scala/com/lt/junto/typeclass/Eq.scala
@@ -5,4 +5,11 @@ trait Eq[A] {
   def neq(a:A, b:A):Boolean = !eq(a, b)
 }
 
+object Eq {
+  def eq[A](lhs:A, rhs:A)(adapter: Eq[A]) = adapter.eq(lhs, rhs)
+  def neq[A](lhs:A, rhs:A)(adapter: Eq[A]) = adapter.neq(lhs, rhs)
 
+  lazy val doubleHasEq = new Eq[Double] {
+    override def eq(a: Double, b: Double) = a == b
+  }
+}
--- Typeclass/src/test/scala/com/it/junto/typeclass/EqTest.scala
+++ Typeclass/src/test/scala/com/it/junto/typeclass/EqTest.scala
@@ -1,11 +1,18 @@
 package com.it.junto.typeclass
 
+import com.lt.junto.typeclass.Eq
 import org.scalatest.{FunSuite, Matchers}
 
 class EqTest extends FunSuite with Matchers {
-  test("Placeholder") {
-    val expected = 1L
-    val actual = 1L
-    actual shouldBe expected
+  test("Test two doubles for default equality") {
+    val expected = 1.0
+    val actual = 1.0
+    assert(Eq.eq(expected, actual)(Eq.doubleHasEq), s"$expected should be equal to $actual")
+  }
+
+  test("Test two doubles for default inequality") {
+    val expected = 1.0
+    val actual = 5.0
+    assert(Eq.neq(expected, actual)(Eq.doubleHasEq), s"$expected should not be equal to $actual")
   }
 }
