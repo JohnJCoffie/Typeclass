6cb3a0749af4cf1b90d2cc4b12dbd4cf2b064110
John Coffie <j_coffie@yahoo.com>
2019-05-26 17:14:50 -0500

    a

--- FreeMonad/src/main/scala/Free.scala
+++ FreeMonad/src/main/scala/Free.scala
@@ -1,3 +1,19 @@
-class Free {
+class Free[F[_], A](implicit FUNC: Functor[F]) {
+  def flatMap[B](f: A => Free[F, B]): Free[F, B] =
+    this match {
+      case Done(a) => f(a)
+      case More(k) => More(FUNC.map(k)(_ flatMap f))
+    }
 
+  def map[B](f: A => B): Free[F, B] = flatMap(x => Done(f(x)))
 }
+
+case class Done[F[_] : Functor, A](a: A) extends Free[F, A]
+
+case class More[F[_] : Functor, A](k: F[Free[F, A]]) extends Free[F, A]
+
+/*
+Free[KVS, Unit] = More(Put(k, v, Done(())))
+Free[KVS, String] = More(Get(k, v => Done(v)))
+Free[KVS, Unit] = More(Delete(k, Done(())))
+ */
--- FreeMonad/src/main/scala/KVS.scala
+++ FreeMonad/src/main/scala/KVS.scala
@@ -1,3 +1,8 @@
-class KVS {
+sealed trait KVS[A]
 
-}
+case class Put[A](key: String, value: String, a: A) extends KVS[A]
+
+case class Get[A](key: String, h: String => A) extends KVS[A]
+
+case class Delete[A](key: String, a: A) extends KVS[A]
+
